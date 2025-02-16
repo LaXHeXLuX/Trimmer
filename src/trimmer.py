927bdd3ffea1fc6804020f39e660a3d6f7cffd49
Laas H. Lumberg <laas.lumberg@gmail.com>
2025-02-16 11:43:37 +0200

    made is_collinear into separate static method

--- src/trimmer.py
+++ src/trimmer.py
@@ -106,9 +106,11 @@ class Trim(bpy.types.PropertyGroup):
         return arr
 
     @staticmethod
+    def is_collinear(p1, p2, p3):
+        return (p2[0] - p1[0]) * (p3[1] - p2[1]) == (p2[1] - p1[1]) * (p3[0] - p2[0])
+
+    @staticmethod
     def compact_points(points):
-        def is_collinear(p1, p2, p3):
-            return (p2[0] - p1[0]) * (p3[1] - p2[1]) == (p2[1] - p1[1]) * (p3[0] - p2[0])
 
         if len(points) < 3:
             return points
@@ -122,7 +124,7 @@ class Trim(bpy.types.PropertyGroup):
             p3 = points[(i + 1) % len(points)]  # Next point (wrap around using modulo)
 
             # Include the point if it's not collinear with its neighbors
-            if not is_collinear(p1, p2, p3):
+            if not Trim.is_collinear(p1, p2, p3):
                 compacted.append(p2)
 
         return compacted
