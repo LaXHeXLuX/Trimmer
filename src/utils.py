7db7ebd6d7dc095bb2a8e9bbc2a3f0ec901b92d0
Laas H. Lumberg <laas.lumberg@gmail.com>
2025-03-13 10:55:05 +0200

    added more utils

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 coefficient = 6
 
 def compare(x1, x2):
@@ -20,7 +22,7 @@ def compare(x1, x2):
 def deepCompare(a1, a2):
     try:
         if len(a1) != len(a2):
-            return False
+            return -1
         
         for i in range(len(a1)):
             elementCompare = deepCompare(a1[i], a2[i])
@@ -52,3 +54,72 @@ def roundList(arr):
         except:
             newArr.append(roundList(a))
     return newArr
+
+def isCollinear(p1, p2, p3):
+    v1 = p2 - p1
+    v2 = p3 - p2
+
+    for i in range(len(v1)):
+        if v1[i] != 0 or v2[i] != 0:
+            return deepCompare(v1 * v2[i], v2 * v1[i]) == 0
+
+    return deepCompare(scaled_v1, scaled_v2) == 0
+
+def pointIsCollinear(points, index):
+    p1 = points[(index - 1) % len(points)]
+    p2 = points[index]
+    p3 = points[(index + 1) % len(points)]
+
+    return isCollinear(p1, p2, p3)
+
+def compactPoints(points):
+    if len(points) < 3:
+        return points
+
+    compacted = []
+
+    for i in range(len(points)):
+        if not pointIsCollinear(points, i):
+            compacted.append(points[i])
+    
+    return compacted
+
+def polygonsToEdges(polygons):
+    edges = {}
+
+    for poly in polygons:
+        for i in range(len(poly)):
+            edge = tuple(sorted((tuple(poly[i]), tuple(poly[(i + 1) % len(poly)]))))
+            if edge in edges:
+                edges[edge] += 1
+            else:
+                edges[edge] = 1
+
+    return edges
+
+def boundaryVertices(polygons):
+    edges = polygonsToEdges(polygons)
+    boundaryEdges = [edge for edge in edges if edges[edge] == 1]
+    edgeMap = {}
+    for a, b in boundaryEdges:
+        edgeMap.setdefault(a, []).append(b)
+        edgeMap.setdefault(b, []).append(a)
+
+    # verify edge map
+    for key in edgeMap:
+        if len(edgeMap[key]) != 2:
+            raise Exception(f"Vertex {key} has {len(edgeMap[key])} unique sides, must be 2!")
+
+    # Order the boundary edges into a continuous loop
+    first = next(iter(edgeMap))
+    boundary = []
+    current = first
+    nextVertex = -1
+
+    while nextVertex != first:
+        nextVertex = edgeMap[current][0]
+        edgeMap[nextVertex].remove(current)
+        boundary.append(list(current))
+        current = nextVertex
+
+    return compactPoints(np.array(boundary))
