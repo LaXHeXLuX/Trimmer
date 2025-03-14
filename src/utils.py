d99cb6455c6d51d4ed6d9d23495a3211f1d188cd
Laas H. Lumberg <laas.lumberg@gmail.com>
2025-03-14 21:25:52 +0200

    Added MVC to utils. Fixed MVC edge weight

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# standard utilities
+
 coefficient = 6
 
 def compare(x1, x2):
@@ -124,10 +126,159 @@ def boundaryVertices(polygons):
 
     return compactPoints(np.array(boundary))
 
+# MVC
+
+def normalise(arr):
+    newArr = []
+    s = sum(arr)
+    for a in arr:
+        newArr.append(a / s)
+    return newArr
+
+def mvcPointOnVertex(n, index):
+    arr = []
+    for i in range(n):
+        arr.append(float(0))
+    arr[index] = float(1)
+    return arr
+
+def mvcPointOnEdgeWeight(n, distance1, distance2, index1):
+    arr = []
+    for i in range(n):
+        arr.append(0)
+    
+    index2 = (index1 + 1) % n
+    dist = distance1 + distance2
+    arr[index1] = distance2 / dist
+    arr[index2] = distance1 / dist
+    return arr
+
+def mvcPointWeight(polygon, point):
+    distances = []
+    tanThetas = []
+
+    for i in range(len(polygon)):
+        vertex = polygon[i]
+        v1 = np.array(vertex) - np.array(point)
+        v1Dist = np.linalg.norm(v1)
+
+        if compare(v1Dist, 0) == 0:
+            return mvcPointOnVertex(len(polygon), i)
+
+        nextVertex = polygon[(i+1) % len(polygon)]
+        v2 = np.array(nextVertex) - np.array(point)
+        v2Dist = np.linalg.norm(v2)
+
+        if compare(v2Dist, 0) == 0:
+            return mvcPointOnVertex(len(polygon), (i+1) % len(polygon))
+        cos = np.dot(v1, v2) / (v1Dist * v2Dist)
+
+        if compare(cos, -1) == 0:
+            return mvcPointOnEdgeWeight(len(polygon), v1Dist, v2Dist, i)
+
+        theta = np.arccos(round(np.dot(v1, v2) / (v1Dist * v2Dist), 6))
+
+        distances.append(v1Dist)
+        tanThetas.append(np.tan(theta / 2))
+
+    weight = []
+    for i in range(len(polygon)):
+        prevTan = tanThetas[(i-1) % len(tanThetas)]
+        tan = tanThetas[i]
+        w = (prevTan + tan) / distances[i]
+        weight.append(round(w, 6))
+
+    return normalise(weight)
+
+def mvcWeights(polygon, points):
+    weights = []
+    for point in points:
+        try:
+            iterator = iter(point[0])
+            weights.append(mvcWeights(polygon, point))
+        except:
+            weights.append(mvcPointWeight(polygon, point))
+
+    return deepToList(weights)
+
+def applyMvcWeight(polygon, weights):
+    newX = 0
+    newY = 0
+    
+    for j in range(len(polygon)):
+        x, y = polygon[j]
+        
+        newX += weights[j] * x
+        newY += weights[j] * y
+
+    return [newX, newY]
+
+def applyMvcWeights(polygon, weights):
+    newPositions = []
+    
+    for i in range(len(weights)):
+        try:
+            newPositions.append(applyMvcWeights(polygon, weights[i]))
+        except:
+            newPositions.append(applyMvcWeight(polygon, weights[i]))
+    
+    return newPositions
+
+# Testing
+
 def test(operation, inputs, output):
     result = operation(*inputs)
     if deepCompare(result, output) != 0:
         raise Exception(f"Test failed with {operation.__name__}({inputs}) = {result} != {output}")
+
+def testMVC(oldPolygon, inputPoints, newPolygon, outputPoints):
+    weights = mvcWeights(oldPolygon, inputPoints)
+    result = applyMvcWeights(newPolygon, weights)
+    if deepCompare(result, outputPoints) != 0:
+        raise Exception(f"Equals test failed: {result} != {outputPoints}")
+
+def runBoundaryVerticesTest():
+    test(
+        boundaryVertices, 
+        [[[[0, 0], [0, 1], [1, 1], [1, 0]]]], 
+        [[0, 0], [0, 1], [1, 1], [1, 0]]
+    )
+    test(
+        boundaryVertices, 
+        [[
+            [[0, 0], [0, 1], [1, 0]],
+            [[0, 1], [1, 1], [1, 0]]
+        ]], 
+        [[0, 0], [0, 1], [1, 1], [1, 0]]
+    )
+    test(
+        boundaryVertices, 
+        [[
+            [[0, 0], [0, 1], [1, 1], [1, 0]],
+            [[1, 1], [2, 1], [2, 0], [1, 0]]
+        ]], 
+        [[0, 0], [0, 1], [2, 1], [2, 0]]
+    )
+
+def runMVCTest():
+    testMVC(
+        [[0, 0], [0, 1], [1, 1], [1, 0]],
+        [[0, 0], [0, 1], [1, 1], [1, 0], [0, 0.5], [0.5, 1], [1, 0.5], [0.5, 0], [0.5, 0.5], [0.2, 0.8], [0.7, 0.3]],
+        [[0, 0], [0, 1], [1, 1], [1, 0]],
+        [[0, 0], [0, 1], [1, 1], [1, 0], [0, 0.5], [0.5, 1], [1, 0.5], [0.5, 0], [0.5, 0.5], [0.2, 0.8], [0.7, 0.3]]
+    )
+    testMVC(
+        [[-2, -1], [-2, 1], [2, 1], [2, -1]],
+        [[-1, 0], [0, 0], [1, 0]],
+        [[-2, -1], [-2, 3], [2, 3], [2, -1]],
+        [[-1, 1], [0, 1], [1, 1]]
+    )
+    testMVC(
+        [[0, 0], [0, 2], [5, 2], [5, 0]],
+        [[[0, 0], [0, 2], [1, 1], [2, 2], [2, 0]], [[2, 2], [5, 2], [5, 0], [2, 0]]],
+        [[0, 0], [0, 4], [15, 4], [15, 0]],
+        [[[0, 0], [0, 4], [3, 2], [6, 4], [6, 0]], [[6, 4], [15, 4], [15, 0], [6, 0]]]
+    )
 
 def runTests():
     # compare
@@ -164,28 +315,10 @@ def runTests():
         [[0, 0], [2, 4], [2, 0]]
     )
 
-    # boundaryVertices
-    test(
-        boundaryVertices, 
-        [[[[0, 0], [0, 1], [1, 1], [1, 0]]]], 
-        [[0, 0], [0, 1], [1, 1], [1, 0]]
-    )
-    test(
-        boundaryVertices, 
-        [[
-            [[0, 0], [0, 1], [1, 0]],
-            [[0, 1], [1, 1], [1, 0]]
-        ]], 
-        [[0, 0], [0, 1], [1, 1], [1, 0]]
-    )
-    test(
-        boundaryVertices, 
-        [[
-            [[0, 0], [0, 1], [1, 1], [1, 0]],
-            [[1, 1], [2, 1], [2, 0], [1, 0]]
-        ]], 
-        [[0, 0], [0, 1], [2, 1], [2, 0]]
-    )
+    runBoundaryVerticesTest()
+
+    runMVCTest()
+
 
 if __name__ == "__main__":
     runTests()
