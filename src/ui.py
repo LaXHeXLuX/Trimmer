fca7752e3a7972c9bda6121347320b666748f4a4
Laas H. Lumberg <laas.lumberg@gmail.com>
2025-04-10 21:34:28 +0300

    prettier but also more complicated

--- src/ui.py
+++ src/ui.py
@@ -87,39 +87,42 @@ class ApplyTrimSettings(bpy.types.Panel):
     bl_region_type = "UI"
     bl_category = "Trimmer"
 
-    def drawFillSettings(self, context):
-        pass
-
-    def drawFitSettings(self, context):
-        pass
-
-    def draw(self, context):
+    def drawFitOption(self, context):
         layout = self.layout
         scene = context.scene
         layout.label(text="Trim fitting options:")
         layout.prop(scene.trim_options, "fitOptions")
-        layout.row()
 
-        if Trimmer.currentApplyOption == 'FILL':
-            self.drawFillSettings(context)
-        elif Trimmer.currentApplyOption in ['FIT', 'FIT_X', 'FIT_Y']:
-            self.drawFitSettings(context)
-
+    def drawFillSettings(self, context):
+        layout = self.layout
+        scene = context.scene
         actionSettingsRow = layout.row()
         
         AbstractOperator.init(actionSettingsRow, 'MIRROR_TRIM')
         AbstractOperator.init(actionSettingsRow, 'ROTATE_TRIM')
 
-        fitOption = scene.trim_options.fitOptions
-        if fitOption == 'FIT_X' or fitOption == 'FIT':
-            layout.label(text="posY:")
-            layout.prop(scene.trim_options, "posY")
-        if fitOption == 'FIT_Y' or fitOption == 'FIT':
-            layout.label(text="posX:")
-            layout.prop(scene.trim_options, "posX")
-
         layout.row()
         AbstractOperator.init(layout, 'CONFIRM_TRIM')
+
+    def drawFitSettings(self, context):
+        layout = self.layout
+        scene = context.scene
+        AbstractOperator.init(layout, 'MIRROR_TRIM')
+        layout.prop(scene.trim_options, "rotation")
+        
+        layout.prop(scene.trim_options, "posX")
+        layout.prop(scene.trim_options, "posY")
+        
+        layout.row()
+        AbstractOperator.init(layout, 'CONFIRM_TRIM')
+
+    def draw(self, context):
+        if Trimmer.currentApplyOption == None:
+            self.drawFitOption(context)
+        if Trimmer.currentApplyOption == 'FILL':
+            self.drawFillSettings(context)
+        elif Trimmer.currentApplyOption in ['FIT', 'FIT_X', 'FIT_Y']:
+            self.drawFitSettings(context)
 
     @staticmethod
     def confirmTrim():
