111074bb283e42d1cf8688ba52bfef9b21da9c4f
Laas H. Lumberg <laas.lumberg@gmail.com>
2025-04-12 10:27:51 +0300

    version adjustment

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Trimmer",
     "author": "Laas Hendrik Lumberg",
-    "version": (1, 0),
+    "version": (0, 1, 0),
     "blender": (2, 80, 0),
     "location": "3D View > Tool Panel",
     "description": "Allows easier positioning of UVs on texture atlas and trim-sheet based texture sheets",
